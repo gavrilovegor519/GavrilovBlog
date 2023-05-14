0d74e82a1b41cd224d16f8ce247ace9aec221fa7
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-05-14 11:53:24 +0300

    Fix large object PostgreSQL bug.

--- src/main/java/com/egor/gavrilovblog/repos/PostsRepo.java
+++ src/main/java/com/egor/gavrilovblog/repos/PostsRepo.java
@@ -1,12 +1,14 @@
 package com.egor.gavrilovblog.repos;
 
 import com.egor.gavrilovblog.entities.Posts;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+@Transactional
 public interface PostsRepo extends JpaRepository<Posts, Long> {
     @Query("SELECT p FROM Posts p ORDER BY p.date DESC")
     List<Posts> findLatest5Posts(Pageable pageable);
