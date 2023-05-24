382990d606dd1ed87a67f535752c695ee5c70986
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-05-24 21:56:26 +0300

    Mini-fixes.

--- src/main/java/com/egor/gavrilovblog/entities/Posts.java
+++ src/main/java/com/egor/gavrilovblog/entities/Posts.java
@@ -10,7 +10,7 @@ public class Posts {
     private @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) long id;
     private @Column(nullable = false, length = 300) String title;
-    private @Column(nullable = false) String text;
+    private @Column(nullable = false, length = 50000) String text;
     @Column(nullable = false)
     private Date date = new Date();
 
